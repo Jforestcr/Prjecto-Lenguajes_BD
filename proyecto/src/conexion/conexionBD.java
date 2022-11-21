52f1dffcfb5dfd36e941ed6f0b85e82504aacb2f
Heysemberg <68712224+Heysemberg@users.noreply.github.com>
2022-11-21 15:34:47 -0600

    Nuevo Proyecto
    Login y muestra de usuarios mediante ID
    @Authors (Fernando Araya, Heisemberg Alfaro, Jeffry Castro)

---
+++ proyecto/src/conexion/conexionBD.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conexionBD {
+    
+    Connection nuevaConexion;
+
+    public conexionBD() {
+    }
+    //Inicia el controlador para conectar con la BD
+    public void iniciarDriver(){
+        String driver = "OracleThin";
+            try {
+                Class.forName(driver);
+            } catch (java.lang.ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Se ha presentado un error al iniciar el driver. El error es: " + ex.getMessage());
+            }
+    }
+    
+        public Connection conectarBD() {
+        final String DATABASE_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+        final String user = "reuz";
+        final String pass = "reuz";
+
+        try {
+            nuevaConexion = DriverManager.getConnection(DATABASE_URL, user, pass);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Se ha presentado un error al conectar con la base de datos. El error es: " + ex.getMessage());
+        }
+        return nuevaConexion;
+    }
+        
+            public void cerrarConexionBD() {
+        try {
+            nuevaConexion.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Se ha presentado un error al cerrar la conexión con la base de datos. El error es: " + ex.getMessage());
+        }
+    }
+}
